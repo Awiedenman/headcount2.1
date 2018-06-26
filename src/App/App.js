56a434ce1c45add72eed80c1ae72d09c107cfd60
Austin.Wiedenman <awiedenman@gmail.com>
2018-06-26 16:11:45 -0600

    Build out Card component structure and add state for schools in App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardContainer } from '../CardContainer/CardContainer';
-
+import DistrictRepository from '../helper';
+import kinderData from '../data/kindergartners_in_full_day_program';
 
 class App extends Component {
+  constructor( props ){
+    super( props );
+    this.state = {
+      allDistricts: {}
+    };
+  }
+
+  componentDidMount() {
+    const allDistricts = new DistrictRepository( kinderData );
+    this.setState({ allDistricts });
+  }
+  
   render() {
     return (
       <div>
--- src/helper.js
+++ src/helper.js
@@ -1,51 +1,51 @@
 export default class DistrictRepository {
-  constructor(data) {
-    this.stats = this.stopDuplicates(data);
+  constructor (schoolData) {
+    this.stats = this.stopDuplicates(schoolData);
   }
 
-  stopDuplicates(data) {
-    const cleanedData = data.reduce((cleanedData, dataItem) => {
-      if(!cleanedData[dataItem.Location]) {
+  stopDuplicates(schoolData) {
+    const cleanedData = schoolData.reduce((cleanedData, dataItem) => {
+      if (!cleanedData[dataItem.Location]) {
         cleanedData[dataItem.Location] = [dataItem];
       } else {
         cleanedData[dataItem.Location].push(dataItem);
       }
-      return cleanedData
+      return cleanedData;
     }, {});
 
     return cleanedData;
   }
 
   findByName(location) {
-    if(!location) {
-      return undefined
+    if (!location) {
+      return undefined;
     }
     const cleanedLocation = location.toUpperCase();
     const foundName = Object.keys(this.stats).reduce((locationObj, district) => {
-      if(district.toUpperCase() === cleanedLocation) {
+      if (district.toUpperCase() === cleanedLocation) {
         locationObj.location = district.toUpperCase();
         locationObj.stats = this.stats[district].reduce((statsObj, year) => {
           const roundedData = Math.round(year.Data * 1000)/1000;
           statsObj[year.TimeFrame] = roundedData || 0;
-          return statsObj
+          return statsObj;
         }, {});
       }
-      return locationObj
-    }, {})
-    if(!foundName.location) {
+      return locationObj;
+    }, {});
+    if (!foundName.location) {
       return undefined;
     }
-    return foundName
+    return foundName;
   }
 
   findAllMatches(location) {
-    if(!location) {
-      return Object.keys(this.stats)
+    if (!location) {
+      return Object.keys(this.stats);
     }
     const foundDistricts = Object.keys(this.stats).filter(district => {
-      return district.toUpperCase().includes(location.toUpperCase())
-    })
-    return foundDistricts
+      return district.toUpperCase().includes(location.toUpperCase());
+    });
+    return foundDistricts;
   }
 
 
