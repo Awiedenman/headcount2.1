0a2017f62c0643b6b0a93f5c6f33c69bb1032972
Austin.Wiedenman <awiedenman@gmail.com>
2018-06-27 16:25:10 -0600

    Reformatted cleaned data for easier rendering

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 export const Card = ( {allDistricts} ) => {
-  const districtsData = Object.keys(allDistricts.stats).map(district => {
-    let dataCard = allDistricts.stats[district].reduce((dataCard, year) => {
+  // const districtsData = Object.keys(allDistricts.stats).map(district => {
+  //   let dataCard = allDistricts.stats[district].reduce((dataCard, year) => {
       
-      return [...dataCard, year.Data];
-    }, [] );
-    return <div>
-            <h1>{district}</h1>
-           { dataCard.map(data => {
-              return <p>{data}</p>
-            })}
-          </div>
-  });
-  return districtsData;
+  //     return [...dataCard, year.Data];
+  //   }, [] );
+  //   return <div>
+  //           <h1>{district}</h1>
+  //          { dataCard.map(data => {
+  //             return <p>{data}</p>
+  //           })}
+  //         </div>
+  // });
+  // return districtsData;
 };
