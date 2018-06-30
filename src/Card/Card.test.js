8370585e5032c176d216ab3eb261e86b04ed01e3
Arram Mandel <airum82@gmail.com>
2018-06-30 13:12:14 -0600

    implemented tests to check for conditional 'clicked' classname

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -3,9 +3,8 @@ import { Card } from './Card';
 import { shallow } from 'enzyme';
 
 describe('Card', () => {
-
-  it('matches a Snapshot of the Card', () => {
     const mockData = {location: 'Colorado', stats : {2004: 0.24, 2005: 0.278, 2006: 0.337, 2007: 0.395}};
+  it('matches a Snapshot of the Card', () => {
     const wrapper = shallow(
       <Card
         key={1}
@@ -15,5 +14,23 @@ describe('Card', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+  it('should have classname of "clicked" if prop clicked is true', () => {
+    const wrapper = shallow(<Card
+        key={1}
+        location={'Colorado'}
+        stats={ mockData }
+        clicked={true}
+      />);
+    expect(wrapper.is('.clicked')).toEqual(true);
+  })
+  it('should not have a classname of "clicked" if prop clicked is false', () => {
+    const wrapper = shallow(<Card
+        key={1}
+        location={'Colorado'}
+        stats={ mockData }
+        clicked={false}
+      />);
+    expect(wrapper.is('.clicked')).toEqual(false);
+  })
 
 });
