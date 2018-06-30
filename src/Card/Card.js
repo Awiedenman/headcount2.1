3f32ccad4ef90ed060f30b46150610771554f1cd
Arram Mandel <airum82@gmail.com>
2018-06-30 12:31:10 -0600

    Changed data p tag to li and removed bullet point styling

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -5,15 +5,15 @@ import './Card.css';
 export const Card = ( {location, stats, addClickedCard, clicked } ) => {
   const districtStats = Object.keys(stats).map((year, index) => {
     return (
-      <p className={stats[year] < .5 ? 'under' : 'over'} key={ index }>{ year } : { stats[year] }</p>
+      <li className={stats[year] < .5 ? 'under' : 'over'} key={ index }>{ year } : { stats[year] }</li>
     );
   });
 
 
   return (
     <div className={`card ${clicked ? 'clicked' : ''}`}  onClick={ ()=> addClickedCard( location ) }>
-      <h1>{location}</h1>
-      { districtStats }
+      <h1>{ location }</h1>
+      <ul>{ districtStats }</ul>
     </div>
   );
 };
